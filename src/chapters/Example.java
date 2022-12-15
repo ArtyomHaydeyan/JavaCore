fab9aba88658e9b42aeffe7ff3d197de37909abe
Artyom Haydeyan.com <artyom.haydeyan93@gmail.com>
2022-12-15 18:51:00 +0400

    add some examples

--- src/chapters/Example.java
+++ src/chapters/Example.java
@@ -3,8 +3,5 @@ package chapters;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        String s = "Anna";
-        System.out.println(s);
     }
-
 }
